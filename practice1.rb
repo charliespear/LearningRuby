22e8b457b1d36a6b153b68066aded073593fa361
charlie spear <charlie.spear@gmail.com>
2012-10-26 07:00:39 -0600

    added data arrays and started if stmts

--- practice1.rb
+++ practice1.rb
@@ -13,6 +13,38 @@ puts "number of digits in your number " + digits.to_s
 
 digit = []
 
+# maybe best approach is to iterate through length.   May just do it via math, ie: divide by 10 or 100 and mod.
+
+#data:
+Ones = [zero, one, two, three, four, five, six, seven, eight, nine]
+tens = [ten, twenty, thirty, forty, fifty, sixty, seventy, eighty, ninty]
+teens = [ten, eleven, twelve, thirteen, fourteen, fifteen, sixteen, seventeen, eighteen, nineteen]
+hundreds = [hundred, thousand, million, billion, trillion]
+connectors = [and, &, -, cents, 00, no-cents] 
+
+
+#? start from left instead of from right.   
+
+if digits > 0 then	
+	#get only right character
+	
+	response = "and " + words[right character]
+if digits > 1 then
+    #get only 2nd right character
+	
+ 	response = "and " + teens[right character] + response
+if digits > 2 then
+	#get only 3rd character
+    
+    response = 
+
+
+unless digits=0
+	puts digits.to_s	
+	digit[digits]=1
+	puts digit[digits].to_s
+end
+
 
 # while x <= digits do
 #    digit[x]=value.right.1
